35c711ccd4d316e2afa3fa75331bbffc586d6d9d
Rudi Theunissen <rudolf.theunissen@gmail.com>
2016-03-05 11:00:39 +1300

    Add pair reference test

--- tests/Map/pairs.php
+++ tests/Map/pairs.php
@@ -22,4 +22,18 @@ trait pairs
         $this->assertEquals($expected, array_map($to_array, $pairs->toArray()));
     }
 
+    public function testPairsAreNotReferences()
+    {
+        $obj = new \stdClass();
+        $obj->state = true;
+
+        $instance = $this->getInstance();
+        $instance->put($obj, 1);
+
+        $pairs = $instance->pairs()->toArray();
+        $pairs[0]->key->state = false;
+
+        $this->assertTrue($obj->state);
+        $this->assertFalse($pairs[0]->key->state);
+    }
 }
