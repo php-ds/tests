9cf4978b6cd2887e076909601dd520641a8b9baf
Rudi Theunissen <rudolf.theunissen@gmail.com>
2016-07-25 03:57:55 +1200

    Add pq copy test to check separation

--- tests/PriorityQueue/copy.php
+++ tests/PriorityQueue/copy.php
@@ -14,4 +14,19 @@ trait copy
         $this->assertEquals($instance->toArray(), $copy->toArray());
         $this->assertEquals(count($instance), count($copy));
     }
+
+    public function testCopyDoesNotAffectSubject()
+    {
+        $instance = $this->getInstance();
+        $instance->push('a', 1);
+        $instance->push('b', 2);
+        $instance->push('c', 3);
+
+        $copy = $instance->copy();
+
+        $instance->pop();
+
+        $this->assertEquals(2, count($instance));
+        $this->assertEquals(3, count($copy));
+    }
 }
