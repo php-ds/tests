51d6f7ba1e166a6c950092615bff3415b0a1a573
Rudi Theunissen <rudolf.theunissen@gmail.com>
2016-08-05 10:41:16 +1200

    Remove collect cycles

--- tests/CollectionTest.php
+++ tests/CollectionTest.php
@@ -10,11 +10,6 @@ abstract class CollectionTest extends \PHPUnit_Framework_TestCase
         srand(1);
     }
 
-    public function tearDownAfterClass()
-    {
-        gc_collect_cycles();
-    }
-
     /**
      * Sample size to test larger operations.
      */
