5b2bdaac873aff2abbcbb49aa52f11b6c02203ce
Rudi Theunissen <rudolf.theunissen@gmail.com>
2016-08-08 10:34:13 +1200

    Remove bootstrap

--- bootstrap.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-// Show whether the extension is loaded or not at the top of the report.
-printf("%s\n", str_repeat('-', 79));
-printf("ds extension loaded: %s\n", extension_loaded('ds') ? 'YES' : 'NO');
-printf("%s\n", str_repeat('-', 79));
