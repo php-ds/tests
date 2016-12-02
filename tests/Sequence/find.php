48084a51e094b03125e12f9de01404644e4b585a
Levi Morrison <levim@php.net>
2016-12-02 14:04:02 -0700

    Fix php-ds/extension#44

--- tests/Sequence/find.php
+++ tests/Sequence/find.php
@@ -8,19 +8,19 @@ trait find
         // initial, value, expected
         return [
 
-            [[], 0, false],
+            [[], 0, null],
 
             [['a'], 'a', 0],
-            [['a'], 'b', false],
+            [['a'], 'b', null],
 
             [['a', 'b'], 'a', 0],
             [['a', 'b'], 'b', 1],
-            [['a', 'b'], 'c', false],
+            [['a', 'b'], 'c', null],
 
             [['a', 'b', 'c'], 'a', 0],
             [['a', 'b', 'c'], 'b', 1],
             [['a', 'b', 'c'], 'c', 2],
-            [['a', 'b', 'c'], 'd', false],
+            [['a', 'b', 'c'], 'd', null],
         ];
     }
 
