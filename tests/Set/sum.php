42e637647e4aa7330e45b8610b5f13f90c0026e2
Simon Podlipsky <simon@podlipsky.net>
2020-05-11 14:35:33 +0200

    Mark testSumObjectCasting as incomplete (for Map and Set)
    Polyfill and ext implementations are different

--- tests/Set/sum.php
+++ tests/Set/sum.php
@@ -38,6 +38,13 @@ trait sum
 
     public function testSumObjectCasting()
     {
+        $this->markTestIncomplete(<<<TEXT
+Polyfill implementation is unable to convert gmp_init(42) to 42.
+Ext's ADD_TO_SUM https://github.com/php-ds/ext-ds/blob/2ddef84d3e9391c37599cb716592184315e23921/src/common.h#L87 is similar to PHP_FUNCTION(array_sum) https://github.com/php/php-src/blob/34f727e63716dfb798865289c079b017812ad03b/ext/standard/array.c#L5868 that is called within polyfill.
+Both call convert_scalar_to_number().
+TEXT
+        );
+
         if ( ! extension_loaded('gmp')) {
             $this->markTestSkipped("GMP extension is not installed");
             return;
